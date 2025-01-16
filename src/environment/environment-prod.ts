95128f2b646b2fd42dd168051f3e03bfa3baa0a6
Kishansinh Parmar <kishansinhp26@gmail.com>
2025-01-16 14:45:59 +0530

    changes done

--- src/environment/environment-prod.ts
+++ src/environment/environment-prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: "http://localhost:4200",
+  url: "http://localhost:4800",
   //  baseUrl : 'http://192.168.1.7:4100',
   baseUrl: 'https://api.westgateithub.in/api/v1'
 };
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
 
-  url: "http://localhost:4200",
+  url: "http://localhost:4800",
   //  baseUrl : 'http://192.168.1.7:4100',
   baseUrl: 'https://api.westgateithub.in/api/v1'
 };
